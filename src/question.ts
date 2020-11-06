44033a879d60b863c252f96f7b41f62738294c41
Frank <fboucher@outlook.com>
2020-11-06 13:02:16 -0500

    quick update Readme

---
+++ src/question.ts
@@ -0,0 +1,17 @@
+export default class Question{
+
+    Text: string;
+    QuestionType: string;
+    Image: string;
+    Value: string;
+    Category: string;
+
+
+    constructor(){
+        this.Text = "";
+        this.QuestionType = "";
+        this.Image = "";
+        this.Value= "";
+        this.Category = "";
+    }
+}
