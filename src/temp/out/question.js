3cdf3d6088eb6733948dd7cdc81f0aece23c8fc0
Frank <fboucher@outlook.com>
2020-11-20 17:05:47 -0500

    Default TypeScript-React app
    required typescript 4.0.5

---
+++ src/temp/out/question.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Question = /** @class */ (function () {
+    function Question() {
+        this.Text = "";
+        this.QuestionType = "";
+        this.Image = "";
+        this.Value = "";
+        this.Category = "";
+    }
+    return Question;
+}());
+exports.default = Question;
+//# sourceMappingURL=question.js.map
