eca01da2684c7211d761ce0f20d91a813b43e13f
Frank <fboucher@outlook.com>
2021-02-12 09:43:09 -0500

    hiding code for now

--- src/triavia-webapp/src/components/categoryList.tsx
+++ src/triavia-webapp/src/components/categoryList.tsx
@@ -10,21 +10,21 @@ type Props = {
     name: string;
 }
 
-const CategoryList:React.FC<Props> = ({
-    id,
-    name,
+// const CategoryList:React.FC<Props> = ({
+//     id,
+//     name,
 
-}) => (
+// }) => (
 
-    <select>
-        // option for "all"
-        //loop
-        <option id={id}>
-            {name}
-        </option>
-        namesArray.map(el => (<option value={el}>{el}</option>))
-        //end loop
-    </select>
-);
- 
-export default CategoryList;
+//     <select>
+//         // option for "all"
+//         //loop
+//         <option id={id}>
+//             {name}
+//         </option>
+//          namesArray.map(el => (<option value={el}>{el}</option>))
+//         //end loop
+//     </select>
+// );
+//  
+// export default CategoryList;
